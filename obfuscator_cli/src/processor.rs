732834c728a44f24ab590a3b37061e096a03cf29
user <user@x1080015.local>
2025-06-08 00:35:02 +0200

    publish 0.2.0 fix

--- obfuscator_cli/src/processor.rs
+++ obfuscator_cli/src/processor.rs
@@ -11,7 +11,7 @@ use syn::{
 };
 
 use globset::{Glob, GlobSetBuilder};
-// use rust_code_obfuscator::utils::generate_obf_suffix;
+use rust_code_obfuscator::utils::generate_obf_suffix;
 
 use crate::config::ObfuscateConfig;
 
