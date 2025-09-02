05bd2fe6de2ea1c1d7c7ec8c9eec2adc63ac0aa8
Mike <michkolenda@gmail.com>
2025-09-02 13:32:43 +0200

    add test for errors mod

--- rust_code_obfuscator_core/src/errors.rs
+++ rust_code_obfuscator_core/src/errors.rs
@@ -18,3 +18,37 @@ impl fmt::Display for ObfuscatorError {
 }
 
 impl error::Error for ObfuscatorError {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn display_encryption_error() {
+        let encryption_err = ObfuscatorError::EncryptionError;
+
+        let displayed = encryption_err.to_string();
+
+        assert! {
+            displayed.contains("Encryption failed :(")
+        }
+    }
+
+    #[test]
+    fn display_inv_file_ext() {
+        let test_file_path = "./test.txt";
+
+        let inv_file_ext_err = ObfuscatorError::InvalidFileExtension {
+            path: PathBuf::from(test_file_path),
+        };
+
+        let displayed = inv_file_ext_err.to_string();
+
+        assert! {
+            displayed.contains(test_file_path),
+            "Expected output contains: '{}',got: {}",
+            test_file_path,
+            displayed
+        }
+    }
+}
